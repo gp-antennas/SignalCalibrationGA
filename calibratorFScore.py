515f3d0719d610ac6a1280475fa44159221f509f
sGourapura <gourapura.2@osu.edu>
2019-06-01 22:08:08 -0400

    This is all code needed for forseeable future

---
+++ calibratorFScore.py
@@ -0,0 +1,15 @@
+"""
+Written by: 	Suren Gourapura
+
+Date Written: 	06/01/2019
+
+Goal:			Provide a fitness score for the calibratorMain.py code
+
+Comments:		There will be a main fitness score function called 
+				FitnessMain. This will serve to choose which fitness score
+				will be used by the code. There will be a dummy fitness 
+				score for now to emulate the real thing.
+"""
+
+import numpy as np
+
---
+++ calibratorUI.py
@@ -0,0 +1,16 @@
+"""
+Written by: 	Suren Gourapura
+
+Date Written: 	06/01/2019
+
+Goal:			Acts as a User Interface to all of the SignalCalibration
+				code. Runs the calibratorMain.py, which in turn accesses the
+				calibratorFScore.py and calibratorGA.py codes to run the
+				whole program. 
+
+Comments:		I wrote this code seperately to simplify the useage of this
+				program. Most knobs to adjust the code are shown here.
+"""
+
+import calibratorMain as main
+
